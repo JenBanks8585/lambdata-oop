ea59c370cf304e359f9352a224180824c848361d
Jennifer Banks <59672512+JenBanks8585@users.noreply.github.com>
2020-05-13 18:24:46 -0400

    Updated to one module

--- README.md
+++ README.md
@@ -7,52 +7,57 @@
     Intallation:
         pip install -i https://test.pypi.org/simple/ Order
 
+    Imports:
+        from helper.menu import Burger, Chili, Menu
 
+
+    Menu Methods:
+        a. Add price of any two available items
+           print(c1 + b2)
+        
+        b. Number of Products available
+           print (Menu.num_of_products)
+        
+        c. Computes discounted price
+           print (b2.apply_discount())
+          
+    
     Chili Methods:
         a. chili_type(self)
-            # function describing chili type, price and calories
+            # function describing chili type, price, calories with or without cheese
 
-        b. with_cheese(self)
-            # function whether it has cheese or not
-
-        c. with_pasta(self)
-            # function whether it has pasta or not
-    
     Burger Methods:
         a. apply_discount(self)
             # function where potential discount can be applied
 
         b. burger_type(self)
-            # function describing burger type, price and calories
+            # function describing burger type, price, calories, with or without lettuce
 
-        c. with_lettuce(self)
-           # function whether it has lettuce or no
-
-
+        
 ## Example Instances:
 
 ### Chili Instances
-   * c1 = Chili('Chili Mac', 4.49, 1200, True, False)
-   * c2 = Chili('Chili Mac Supreme', 4.99, 1410, True, True)
-   * c3 = Chili('Chili 3-Way', 4.99, 710, True, False)
-   * c4 = Chili('Chili 5- Way', 4.99, 1160, True, True)
-   * c5 = Chili('Genuine Chili', 4.99, 550, False, False)
-   * c6 = Chili('Chili Deluxe', 4.99, 1000, False, False)
+   * c1 = Chili('Chili Mac', 4.49, 1200, 'With Cheese')
+   * c2 = Chili('Chili Mac Supreme', 4.99, 1410, 'No Cheese')
+   * c3 = Chili('Chili 3-Way', 4.99, 710, 'With Cheese')
+   * c4 = Chili('Chili 5- Way', 4.99, 1160, 'With Cheese')
+   * c5 = Chili('Genuine Chili', 4.99, 550, 'No Cheese')
+   * c6 = Chili('Chili Deluxe', 4.99, 1000, 'With Cheese')
 
 ### Burger instances
-   * b1 = Burger('Pork Belly Double', 6.49, 740, False)
-   * b2 = Burger('Original Double n Cheese', 3.99, 770, True)
-   * b3 = Burger('Single n Cheese', 3.79, 630, True)
-   * b4 = Burger('Triple Steakburger n Fries', 3.99, 850, True)
-  * b5 = Burger('Frisco Melt n Fries', 5.49, 1200, False)
+   * b1 = Burger('Pork Belly Double', 6.49, 740, 'With Lettuce')
+   * b2 = Burger('Original Double n Cheese', 3.99, 770, 'With Lettuce')
+   * b3 = Burger('Single n Cheese', 3.79, 630, 'No Lettuce')
+   * b4 = Burger('Triple Steakburger n Fries', 3.99, 850, 'No Lettuce')
+   * b5 = Burger('Frisco Melt n Fries', 5.49, 1200, 'With Lettuce')
 
 ## Sample results
   * print (b1.burger_type())
-  * print (b1.with_lettuce())
   * print(b2.price)
   * print (b2.apply_discount())
   * print (Menu.num_of_products)
+
   * print (c1.chili_type())
-  * print (c2.with_cheese())
   * print(c2.price)
-  * print (c3.with_cheese())
+  * print ('Price of two items is ' , c2 + b1)
+  * print (c2.cheese)
--- helper/menu.py
+++ helper/menu.py
@@ -1,105 +1,137 @@
-"""
-class Menu 
-        name, price, calory
-"""
 
 class Menu:
 
-    # class variables
-    num_of_products = 0
-    discount = 0.95
+  """
+## Steak N' Shake helpers with OOP
+    This is a subset of Steak N' Shake menu that covers 
+        a. Burgers
+        b. Chili
 
-    def __init__(self, name, price, calory):
+    Intallation:
+        pip install -i https://test.pypi.org/simple/ Order
+
+    Imports:
+        from helper.menu import Burger, Chili, Menu
+
+
+    Menu Methods:
+        a. Add price of any two available items
+           print(c1 + b2)
+        
+        b. Number of Products available
+           print (Menu.num_of_products)
+        
+        c. Computes discounted price
+           print (b2.apply_discount())
+"""
+
+  # class variables
+  num_of_products = 0
+  discount = 0.95
+
+  def __init__(self, name, price, calory):
         self.name = name
         self.price = price
         self.calory = calory
 
         Menu.num_of_products += 1
+    
+  # dunder method to add any two available items' prices
+  def __add__(self, other):
+      return self.price + other.price
 
-"""
-class Burger inherits Menu attributes and class variables.
-"""
 
 class Burger(Menu):
-    
-    def __init__(self, name, price, calory, lettuce):
+  
+  """
+class Burger inherits Menu attributes and class variables.
+
+ Burger Methods:
+    a. apply_discount(self)
+        # function where potential discount can be applied
+
+    b. burger_type(self)
+        # function describing burger type, price and calories
+
+    Sample results:
+        print (b1.burger_type())
+        print(b2.price)
+        print(b2.name)
+        print(b4.lettuce)
+"""
+  
+  def __init__(self, name, price, calory, lettuce):
         super(). __init__(name, price, calory)
+        self.lettuce = lettuce
 
     # function describing burger type, price and calories
-    def burger_type(self):
+  def burger_type(self):
         return '{} {} {} {} {}'. format(self.name,
                                         '$', self.price, 
                                         self.calory, 'cal')
 
-    # function whether it has lettuce or not  
-    def with_lettuce(self):
-        self.lettuce = True
-        if self. lettuce is True:
-            return "This burger has lettuce"
-        else:
-            return "This burger does NOT have lettuce"
-
     # function where potential discount can be applied
-    def apply_discount(self):
+  def apply_discount(self):
         self.price= float(self.price * self.discount)
         return '{} {}'. format('Discounted price is: $', self.price) 
 
-"""
-class Chili inherits Menu attributes and class variables.
-"""
 
 class Chili(Menu):
-        
-    def __init__(self, name, price, calory, pasta, cheese):
+
+  """
+class Chili inherits Menu attributes and class variables.
+
+Chili Methods:
+    a. chili_type(self)
+        # function describing chili type, price, calories with or without cheese
+
+    Sample results:
+        print (c1.chili_type())
+        print(c2.price)
+        print(c2.name)
+        print(c4.cheese)
+  """
+  
+  def __init__(self, name, price, calory, cheese):
         super(). __init__(name, price, calory)
+        self.cheese = cheese
 
     # function describing chili type, price and calories
-    def chili_type(self):
+  def chili_type(self):
         return '{} {} {} {} {}'. format(self.name,
                                         '$', self.price, 
                                         self.calory, 'cal')
-            
-    # function whether it has cheese or not  
-    def with_cheese(self):
-        self.cheese = True
-        if self.cheese is True:
-            return "This chili has cheese"
-        else:
-            return "This chili does NOT have cheese"
-    
-    # function whether it has pasta or not  
-    def with_pasta(self):
-        self.pasta = True
-        if True:
-            return "This chili has pasta"
-        else:
-            return "This chili does NOT have pasta"
+        
+          
+if __name__ == "__main__":
 
-"""
-#Chili Instances
-c1 = Chili('Chili Mac', 4.49, 1200, True, False)
-c2 = Chili('Chili Mac Supreme', 4.99, 1410, True, True)
-c3 = Chili('Chili 3-Way', 4.99, 710, True, False)
-c4 = Chili('Chili 5- Way', 4.99, 1160, True, True)
-c5 = Chili('Genuine Chili', 4.99, 550, False, False)
-c6 = Chili('Chili Deluxe', 4.99, 1000, False, False)
+  def __add__(self, other):
+      return self.price + other.price
+   
+
+""" #Chili Instances
+  c1 = Chili('Chili Mac', 4.49, 1200, 'With Cheese')
+  c2 = Chili('Chili Mac Supreme', 4.99, 1410, 'No Cheese')
+  c3 = Chili('Chili 3-Way', 4.99, 710, 'With Cheese')
+  c4 = Chili('Chili 5- Way', 4.99, 1160, 'With Cheese')
+  c5 = Chili('Genuine Chili', 4.99, 550, 'No Cheese')
+  c6 = Chili('Chili Deluxe', 4.99, 1000, 'With Cheese')
 
 # Burger instances
-b1 = Burger('Pork Belly Double', 6.49, 740, False)
-b2 = Burger('Original Double n Cheese', 3.99, 770, True)
-b3 = Burger('Single n Cheese', 3.79, 630, True)
-b4 = Burger('Triple Steakburger n Fries', 3.99, 850, True)
-b5 = Burger('Frisco Melt n Fries', 5.49, 1200, False)
+  b1 = Burger('Pork Belly Double', 6.49, 740, 'With Lettuce')
+  b2 = Burger('Original Double n Cheese', 3.99, 770, 'With Lettuce')
+  b3 = Burger('Single n Cheese', 3.79, 630, 'No Lettuce')
+  b4 = Burger('Triple Steakburger n Fries', 3.99, 850, 'No Lettuce')
+  b5 = Burger('Frisco Melt n Fries', 5.49, 1200, 'With Lettuce')
 
-# Sample results
-print (b1.burger_type())
-print (b1.with_lettuce())
-print(b2.price)
-print (b2.apply_discount())
-print (Menu.num_of_products)
+  
+  # Sample results
+  print (b1.burger_type())
+  print(b2.price)
+  print (b2.apply_discount())
+  print (Menu.num_of_products)
 
-print (c1.chili_type())
-print (c2.with_cheese())
-print(c2.price)
-print (c3.with_cheese())
-"""
+  print (c1.chili_type())
+  print(c2.price)
+  print ('Price of two items is ' , c2 + b1)
+  print (c2.cheese) """
